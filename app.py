11ee3efec767ea6bc30bbd86b1f358aff513bc94
Kamil Khan <routetokamil@gmail.com>
2023-02-21 17:26:15 +0530

    Integrated with sql lite Made to show list of songs by adding dummy entries in sql lite database

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask import render_template
+import sqlite3
 
 app = Flask(__name__)
+
+
+@app.route('/home')
+def home():
+    conn = sqlite3.connect('music')
+    items = list()
+    cursor = conn.execute("SELECT album,title,artist, uuid from songs")
+    for row in cursor:
+        item = dict()
+        item['album'] = row[0]
+        item['title'] = row[1]
+        item['artist'] = row[2]
+        items.append(item)
+    conn.close()
+    return render_template('home.html',items=items)
+
 
 @app.route("/")
 def root():
