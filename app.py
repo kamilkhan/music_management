0b96e00bd74cb51cbc6461d41df00c49e2f0d7c2
Kamil Khan <routetokamil@gmail.com>
2023-02-22 08:01:54 +0530

    removed unnecessary prints

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
 from flask import render_template,Flask,request,redirect, send_from_directory
 from werkzeug.utils import secure_filename
-import sqlite3
+import db
 import os
 import uuid
 
-app = Flask(__name__, static_folder='static')
+
+def create_app(test_config=None):
+    music_app = Flask(__name__, instance_relative_config=True)
+    music_app.config.from_mapping(
+        SECRET_KEY='dev',DATABASE=os.path.join(music_app.instance_path, 'music'),
+    )
+    if test_config is None:
+        music_app.config.from_pyfile('config.py', silent=True)
+    else:
+        music_app.config.from_mapping(test_config)
+    try:
+        os.makedirs(music_app.instance_path)
+    except OSError:
+        pass
+    db.init_app(music_app)
+    return music_app
+
+
+app = create_app()
 
 
 @app.route('/home')
 def home():
-    conn = sqlite3.connect('music')
+    conn = db.get_db()
     cursor = conn.execute("SELECT album,title,artist,uuid from songs")
     items = make_items(cursor)
-    conn.close()
     return render_template('home.html', items=items,no_of_songs=len(items))
 
 
@@ -21,12 +38,11 @@ def search():
     search_token = request.form.get('search')
     if search_token == '':
         return redirect("/home", code=302)
-    conn = sqlite3.connect('music')
+    conn = db.get_db()
     query = "select album,title,artist,uuid from songs where album like '%" + search_token \
             + "%' or title like '%" + search_token + "%' or artist like '%" + search_token + "%'"
     cursor = conn.execute(query)
     items = make_items(cursor)
-    conn.close()
     return render_template('home.html', items=items, search_text=search_token, no_of_songs=len(items))
 
 
@@ -55,32 +71,28 @@ def upload():
             return render_template("upload_song_form.html", error_message = "Please select Audio File")
         uuid_string = str(uuid.uuid1())
         fname = uuid_string + ".mp3"
-        if not os.path.isdir(os.getcwd() + "/static/songs"):
-            os.makedirs(os.getcwd() + "/static/songs")
-        f.save(os.getcwd() + "/static/songs/"+fname)
-        conn = sqlite3.connect('music')
+        f.save(os.getcwd() + "/static/"+fname)
+        conn = db.get_db()
         cur = conn.cursor()
         cur.execute("INSERT INTO songs (uuid,album,title,artist) values (?,?,?,?)", (uuid_string, album, title, artist))
         conn.commit()
-        conn.close()
     return redirect("/home", code=302)
 
 
 @app.route("/delete/<string:uuid>", methods=["POST"])
 def delete(uuid):
-    conn = sqlite3.connect('music')
+    conn = db.get_db()
     cur = conn.cursor()
     cur.execute("DELETE from songs where uuid = '" + uuid + "'")
     conn.commit()
-    conn.close()
-    os.remove("static/songs/" + uuid + ".mp3")
+    os.remove(os.getcwd() + "/static/" + uuid + ".mp3")
     return redirect("/home", code=302)
 
 
 @app.route('/download/<path:uuid>', methods=['GET'])
 def download(uuid):
     filename = uuid + ".mp3"
-    return send_from_directory(directory='static/songs', path=filename, as_attachment=True)
+    return send_from_directory(directory='static', path=filename, as_attachment=True)
 
 
 @app.route('/play/<path:uuid>', methods=['GET'])
--- db.py
+++ db.py
@@ -26,7 +26,6 @@ def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
-        print("SCHE")
         db.executescript(f.read().decode('utf8'))
 
 
